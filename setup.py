2b4b11befad528a1710af26c23368a97a28d120d
Miron Jakubowski <mijakubowski@gmail.com>
2017-06-28 21:37:54 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name="norimdb",
+      version="0.9.0-alpha.sqlite",
+      description="Python 3.x simple file database",
+      author="Miron Jakubowski",
+      author_email="mijakubowski@gmail.com",
+      url="https://github.com/meeron/norimdb",
+      keywords=["database", "nosql"],
+      license="MIT",
+      packages=["norimdb"]
+      )
